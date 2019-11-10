585a364888ae34db55aa4efef9da1d0a6725e6d1
Alexander Puzynia <werwolf.by@gmail.com>
2019-11-10 12:35:41 -0800

    feat: implement find_network_dependencies method

--- dhealer/healer.py
+++ dhealer/healer.py
@@ -15,3 +15,17 @@ class Healer:
             state = container.attrs['State']
             if 'Health' in state and state['Health']['Status'] == 'unhealthy':
                 container.restart()
+
+    def find_network_dependencies(self, id):
+        container_network_mode_prefix = "container:"
+
+        container_ids = []
+
+        containers = self.client.containers.list()
+        for container in containers:
+            network_mode = container.attrs['HostConfig']['NetworkMode']
+            if network_mode.startswith(container_network_mode_prefix) and network_mode[len(container_network_mode_prefix):].lower() == id.lower():
+                container_ids.append(container.id.lower())
+
+        return container_ids
+
--- tests/test_healer.py
+++ tests/test_healer.py
@@ -6,26 +6,12 @@ from dhealer.healer import Healer
 class TestHealer:
     @pytest.mark.parametrize("healthy", [True, False])
     def test_heal_containers(self, healthy):
-        client = Mock()
-        client.containers = PropertyMock()
+        (client, container1, container2) = TestHealer._init()
         container1 = Mock()
-        container1.attrs = {
-            'State': {
-                'Status': 'running',
-            },
-        }
 
-        container2 = Mock()
-        container2.attrs = {
-            'State': {
-                'Status': 'running',
-                'Health': {
+        container2.attrs['State']['Health'] = {
                     "Status": "healthy" if healthy else "unhealthy",
-                },
-            },
-        }
-
-        client.containers.list = Mock(return_value=[container1, container2])
+                }
 
         healer = Healer(client)
         healer.heal_containers()
@@ -35,3 +21,52 @@ class TestHealer:
             container2.restart.assert_not_called()
         else:
             container2.restart.assert_called()
+
+    def test_find_network_dependencies_no_dependencies(self):
+        (client, container1, container2) = TestHealer._init()
+
+        healer = Healer(client)
+        dependencies = healer.find_network_dependencies(container1.id)
+
+        assert len(dependencies) == 0
+
+    def test_find_network_dependencies_single_dependencies(self):
+        (client, container1, container2) = TestHealer._init()
+
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+
+        healer = Healer(client)
+        dependencies = healer.find_network_dependencies(container1.id)
+
+        assert len(dependencies) == 1
+        assert dependencies[0] == container2.id
+
+    @staticmethod
+    def _init():
+        container1 = Mock()
+        container1.id = '1111'
+        container1.attrs = {
+            'State': {
+                'Status': 'running',
+            },
+            'HostConfig': {
+                'NetworkMode': 'host',
+            },
+        }
+
+        container2 = Mock()
+        container2.id = '2222'
+        container2.attrs = {
+            'State': {
+                'Status': 'running',
+            },
+            'HostConfig': {
+                'NetworkMode': 'host',
+            },
+        }
+
+        client = Mock()
+        client.containers = PropertyMock()
+        client.containers.list = Mock(return_value=[container1, container2])
+
+        return client, container1, container2
