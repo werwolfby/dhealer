6b363465ee35b887da64d7680773cc8f2a5cfee8
Alexander Puzynia <werwolf.by@gmail.com>
2019-11-10 23:41:04 -0800

    feat: implement deep traverse of container dependencies to run restart in right order

--- tests/test_healer.py
+++ tests/test_healer.py
@@ -41,32 +41,130 @@ class TestHealer:
         assert len(dependencies) == 1
         assert dependencies[0] == container2.id
 
-    @staticmethod
-    def _init():
-        container1 = Mock()
-        container1.id = '1111'
-        container1.attrs = {
-            'State': {
-                'Status': 'running',
-            },
-            'HostConfig': {
-                'NetworkMode': 'host',
-            },
-        }
+    def test_reorder_dependencies_no_dependencies(self):
+        (client, container1, container2) = TestHealer._init()
 
-        container2 = Mock()
-        container2.id = '2222'
-        container2.attrs = {
-            'State': {
-                'Status': 'running',
-            },
-            'HostConfig': {
-                'NetworkMode': 'host',
-            },
-        }
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id])
+
+        assert len(dependencies) == 1
+        assert dependencies[0] == container1.id
+
+    def test_reorder_dependencies_single_dependencies(self):
+        (client, container1, container2) = TestHealer._init()
+
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id])
+
+        assert len(dependencies) == 2
+        assert dependencies[0] == container1.id
+        assert dependencies[1] == container2.id
+
+    def test_reorder_dependencies_multiple_dependencies(self):
+        (client, container1, container2, container3) = TestHealer._init(3)
+
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+        container3.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id])
+
+        assert len(dependencies) == 3
+        assert dependencies[0] == container1.id
+        assert dependencies[1] == container2.id
+        assert dependencies[2] == container3.id
+
+    def test_reorder_dependencies_multiple_deep_dependencies(self):
+        (client, container1, container2, container3) = TestHealer._init(3)
+
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+        container3.attrs['HostConfig']['NetworkMode'] = 'container:' + container2.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id])
+
+        assert len(dependencies) == 3
+        assert dependencies[0] == container1.id
+        assert dependencies[1] == container2.id
+        assert dependencies[2] == container3.id
+
+    def test_reorder_dependencies_multiple_unhealthy_different_dependencies(self):
+        (client, container1, container2, container3, container4) = TestHealer._init(4)
+
+        container1.attrs['State']['Health'] = {
+                    "Status": "unhealthy",
+                }
+        container3.attrs['State']['Health'] = {
+                    "Status": "unhealthy",
+                }
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container1.id
+        container4.attrs['HostConfig']['NetworkMode'] = 'container:' + container3.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id, container3.id])
+
+        assert len(dependencies) == 4
+        assert dependencies[0] == container1.id
+        assert dependencies[1] == container2.id
+        assert dependencies[2] == container3.id
+        assert dependencies[3] == container4.id
+
+    def test_reorder_dependencies_cross_dependencies(self):
+        (client, container1, container2, container3) = TestHealer._init(3)
+
+        container1.attrs['HostConfig']['NetworkMode'] = 'container:' + container3.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container1.id, container3.id])
+
+        assert len(dependencies) == 2
+        assert dependencies[0] == container3.id
+        assert dependencies[1] == container1.id
+
+    def test_reorder_dependencies_multiple_cross_deep_dependencies(self):
+        (client, container1, container2, container3, container4) = TestHealer._init(4)
+
+        container1.attrs['HostConfig']['NetworkMode'] = 'container:' + container2.id
+        container2.attrs['HostConfig']['NetworkMode'] = 'container:' + container4.id
+        container3.attrs['HostConfig']['NetworkMode'] = 'container:' + container4.id
+
+        healer = Healer(client)
+        dependencies = healer.reorder_dependencies([container4.id])
+
+        assert len(dependencies) == 4
+        assert dependencies[0] == container4.id
+        assert dependencies[1] == container2.id
+        assert dependencies[2] == container1.id
+        assert dependencies[3] == container3.id
+
+    @staticmethod
+    def _init(count=2):
+        containers = []
+
+        for i in range(count):
+            container = Mock()
+            container.id = str(i + 1) * 6
+            container.attrs = {
+                'State': {
+                    'Status': 'running',
+                },
+                'HostConfig': {
+                    'NetworkMode': 'host',
+                },
+            }
+            containers.append(container)
+
+        def get_container(id):
+            for container in containers:
+                if container.id == id:
+                    return container
+            raise KeyError('Can\'t find container by id')
 
         client = Mock()
         client.containers = PropertyMock()
-        client.containers.list = Mock(return_value=[container1, container2])
+        client.containers.list = Mock(return_value=containers)
+        client.containers.get = Mock(side_effect=get_container)
 
-        return client, container1, container2
+        return tuple([client] + containers)
